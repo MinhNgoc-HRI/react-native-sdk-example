690e25234065099eb743144356fae89d69048210
ngnm1009 <ngnm1009@gmail.com>
2023-03-17 14:45:42 +0700

    sdk

---
+++ Frameworks/RNSDK.xcframework/ios-arm64_x86_64-simulator/RNSDK.framework/Headers/RNSDK.h
@@ -0,0 +1,14 @@
+//
+//  RNSDK.h
+//  RNSDK
+//
+//  Created by Minh Ngọc on 17/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+FOUNDATION_EXPORT double RNSDKVersionNumber;
+
+//! Project version string for RNSDK.
+FOUNDATION_EXPORT const unsigned char RNSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNSDK/PublicHeader.h>
